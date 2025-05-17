d3382565eafa83be4d99cafda9c8c6aad3fa1860
violaine soulas <vhsoulas@gmail.com>
2025-05-17 19:04:08 +0200

    Modif configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               'family-loc.store',
+    user_name:            ENV['GMAIL_ADDRESS'],
+    password:             ENV['GMAIL_APP_PASSWORD'],
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
+config.action_mailer.default_url_options = { host: 'www.family-loc.store', protocol: 'https' }
+
   config.action_mailer.default_url_options = { host: "www.family-loc.store" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: 'gmail.com',
+  domain: 'family-loc.store',
   user_name: ENV['GMAIL_ADDRESS'],
   password: ENV['GMAIL_APP_PASSWORD'],
-  authentication: :login,
+  authentication: :plain,
   enable_starttls_auto: true
 }
