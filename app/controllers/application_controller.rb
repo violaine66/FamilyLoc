4bb6d8cac257ef83faaebf5123de70fa4aadf527
violaine soulas <vhsoulas@gmail.com>
2025-05-23 08:40:01 +0200

    Ajout nom complet aux forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit::Authorization
 
   after_action :verify_authorized, if: -> { !skip_pundit? && action_name != 'index' }
@@ -7,6 +8,11 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     proprietes_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nom_complet])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nom_complet])
   end
 
   private
