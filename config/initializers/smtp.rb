f04d2745d783286b2fe62db3741862858f117278
violaine soulas <vhsoulas@gmail.com>
2025-06-01 15:37:38 +0200

    ajout smtp

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -7,3 +7,12 @@
 #   authentication: :plain,
 #   enable_starttls_auto: true
 # }
+ ActionMailer::Base.smtp_settings  = {
+    address:              "smtp-relay.brevo.com",
+    port:                 587,
+    domain:               'family-loc.store',
+    user_name:            ENV['USERNAME'],
+    password:             ENV['PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
