9e8862a3f6aad99bee5de4edb1f1920b0521d7cf
violaine soulas <vhsoulas@gmail.com>
2025-06-01 12:51:02 +0200

    Comment smtp settings dans initializers

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
-ActionMailer::Base.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: 'family-loc.store',
-  user_name: ENV['GMAIL_ADDRESS'],
-  password: ENV['GMAIL_APP_PASSWORD'],
-  authentication: :plain,
-  enable_starttls_auto: true
-}
+# ActionMailer::Base.smtp_settings = {
+#   address: "smtp.gmail.com",
+#   port: 587,
+#   domain: 'family-loc.store',
+#   user_name: ENV['GMAIL_ADDRESS'],
+#   password: ENV['GMAIL_APP_PASSWORD'],
+#   authentication: :plain,
+#   enable_starttls_auto: true
+# }
