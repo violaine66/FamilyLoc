c33a46e30b0304b748222dffce5aea07c80771dd
violaine soulas <vhsoulas@gmail.com>
2025-09-27 15:29:24 +0200

    modif nom du domaine dans les paramètres d'envoi des mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
     port:                 587,
-    domain:               'family-loc.store',
+    domain:               'gmail.com',
     user_name:            ENV['GMAIL_ADDRESS'],
     password:             ENV['GMAIL_APP_PASSWORD'],
     authentication:       :plain,
