d826da0fb43277ff4b7cee2d2204635384588f23
violaine soulas <vhsoulas@gmail.com>
2025-09-27 19:44:36 +0200

    suppression de smtp.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,8 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
- config.mailer = 'Devise::Mailer'
-  config.parent_mailer = 'ActionMailer::Base'
+  config.mailer_sender = 'contact@family-loc.store'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
