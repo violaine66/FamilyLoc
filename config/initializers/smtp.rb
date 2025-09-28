3720e57e9a080a20ff75f4ef9cbe5f1366dde5b8
violaine soulas <vhsoulas@gmail.com>
2025-09-28 11:04:02 +0200

    Ajout du dossier smtp

---
+++ config/initializers/smtp.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings  = {
+   ddress:              'smtp.zoho.com',
+    port:                 587,
+    domain:               'family-loc.store',
+    user_name:            ENV['user_name'],   # ⚠️ doit être bien défini sur Heroku
+    password:             ENV['password'],    # ⚠️ idem
+    authentication:       :plain,
+    enable_starttls_auto: true
+}
