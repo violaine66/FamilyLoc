433b2c9237f234bbd8f054943d839c5059484427
violaine soulas <vhsoulas@gmail.com>
2025-09-28 18:13:55 +0200

    Suppression TestMailer

--- app/mailers/test_mailer.rb
+++
@@ -1,9 +0,0 @@
-# class TestMailer < ApplicationMailer
-#   default from: 'soulas@family-loc.store'
-
-#   def test_email(to_email)
-#     mail(to: to_email, subject: 'Test Action Mailer') do |format|
-#       format.text { render plain: "Ceci est un email de test." }
-#     end
-#   end
-# end
