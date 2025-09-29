11fa003eee2dd63db057949e5ccfc4f16db78cce
violaine soulas <vhsoulas@gmail.com>
2025-09-29 09:43:09 +0200

    Ajout date fin qui s'ouvre à partir de la date debut sur la saisie des réservations

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,5 +1,53 @@
-import { Controller } from "@hotwired/stimulus";
-import flatpickr from "flatpickr";
+// import { Controller } from "@hotwired/stimulus";
+// import flatpickr from "flatpickr";
+
+// // Connects to data-controller="datepicker"
+// export default class extends Controller {
+//   static values = { bookings: Array } // [{start: "YYYY-MM-DD", end: "YYYY-MM-DD"}, ...]
+
+//   connect() {
+//     const FRENCH_LOCALE = {
+//       weekdays: {
+//         shorthand: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+//         longhand: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+//       },
+//       months: {
+//         shorthand: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Jui', 'Juil', 'Aoû', 'Sep', 'Oct', 'Nov', 'Déc'],
+//         longhand: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+//       },
+//       firstDayOfWeek: 1,
+//       rangeSeparator: ' au ',
+//       weekAbbreviation: 'Sem',
+//       scrollTitle: 'Défiler pour augmenter la valeur',
+//       toggleTitle: 'Cliquer pour basculer',
+//     };
+
+//     // Construire les plages désactivées à partir des réservations
+//     const disabledRanges = this.bookingsValue.map(booking => ({
+//       from: booking.start,
+//       to: booking.end
+//     }));
+
+//     // Initialiser Flatpickr sur tous les inputs type text du form
+//     flatpickr(this.element.querySelectorAll("input[name*='date']"), {
+//       minDate: "today",
+//       altInput: true,
+//       locale: FRENCH_LOCALE,
+//       altFormat: "d-m-Y",
+//       dateFormat: "Y-m-d",
+//       disable: disabledRanges,
+//       onDayCreate: (dObj, dStr, fp, dayElem) => {
+//         const date = dayElem.dateObj.toISOString().split('T')[0];
+//         // Ajouter la classe "reserved" pour le style si la date est dans une réservation
+//         if (this.bookingsValue.some(b => date >= b.start && date <= b.end)) {
+//           dayElem.classList.add("reserved");
+//         }
+//       }
+//     });
+//   }
+// }
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr"
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
@@ -8,28 +56,32 @@ export default class extends Controller {
   connect() {
     const FRENCH_LOCALE = {
       weekdays: {
-        shorthand: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
-        longhand: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+        shorthand: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+        longhand: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
       },
       months: {
-        shorthand: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Jui', 'Juil', 'Aoû', 'Sep', 'Oct', 'Nov', 'Déc'],
-        longhand: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        shorthand: ["Jan", "Fév", "Mar", "Avr", "Mai", "Jui", "Juil", "Aoû", "Sep", "Oct", "Nov", "Déc"],
+        longhand: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
       },
       firstDayOfWeek: 1,
-      rangeSeparator: ' au ',
-      weekAbbreviation: 'Sem',
-      scrollTitle: 'Défiler pour augmenter la valeur',
-      toggleTitle: 'Cliquer pour basculer',
-    };
+      rangeSeparator: " au ",
+      weekAbbreviation: "Sem",
+      scrollTitle: "Défiler pour augmenter la valeur",
+      toggleTitle: "Cliquer pour basculer",
+    }
 
-    // Construire les plages désactivées à partir des réservations
+    // Construire les plages désactivées
     const disabledRanges = this.bookingsValue.map(booking => ({
       from: booking.start,
       to: booking.end
-    }));
+    }))
 
-    // Initialiser Flatpickr sur tous les inputs type text du form
-    flatpickr(this.element.querySelectorAll("input[name*='date']"), {
+    // Récupération des inputs
+    const startInput = this.element.querySelector("input[name*='date_debut']")
+    const endInput = this.element.querySelector("input[name*='date_fin']")
+
+    // Flatpickr sur date_debut
+    const startCalendar = flatpickr(startInput, {
       minDate: "today",
       altInput: true,
       locale: FRENCH_LOCALE,
@@ -37,12 +89,34 @@ export default class extends Controller {
       dateFormat: "Y-m-d",
       disable: disabledRanges,
       onDayCreate: (dObj, dStr, fp, dayElem) => {
-        const date = dayElem.dateObj.toISOString().split('T')[0];
-        // Ajouter la classe "reserved" pour le style si la date est dans une réservation
+        const date = dayElem.dateObj.toISOString().split("T")[0]
         if (this.bookingsValue.some(b => date >= b.start && date <= b.end)) {
-          dayElem.classList.add("reserved");
+          dayElem.classList.add("reserved")
+        }
+      },
+      onChange: (selectedDates) => {
+        if (selectedDates.length > 0) {
+          const startDate = selectedDates[0]
+          endCalendar.set("minDate", startDate)   // empêche de choisir avant
+          endCalendar.jumpToDate(startDate)       // ouvre directement sur cette date
         }
       }
-    });
+    })
+
+    // Flatpickr sur date_fin
+    const endCalendar = flatpickr(endInput, {
+      minDate: "today",
+      altInput: true,
+      locale: FRENCH_LOCALE,
+      altFormat: "d-m-Y",
+      dateFormat: "Y-m-d",
+      disable: disabledRanges,
+      onDayCreate: (dObj, dStr, fp, dayElem) => {
+        const date = dayElem.dateObj.toISOString().split("T")[0]
+        if (this.bookingsValue.some(b => date >= b.start && date <= b.end)) {
+          dayElem.classList.add("reserved")
+        }
+      }
+    })
   }
 }
